471d0bdce2e1a3dc6040d455315c95e521cd8367
ganjikrishnareddy <reddykrishna35609@gmail.com>
2025-04-07 01:18:19 +0000

    updateddd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/news_mag/', // <-- IMPORTANT for GitHub Pages subfolder!
+  base: '/news_mag/', // 👈 important for GitHub Pages
 });
